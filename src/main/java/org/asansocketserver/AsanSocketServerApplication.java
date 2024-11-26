1e277f55a5859ea698548447146c54269e73617d
Jedo0224 <qkrwotjr0@gmail.com>
2024-11-26 20:26:06 +0900

    [test] 젠킨스 테스트

--- src/main/java/org/asansocketserver/AsanSocketServerApplication.java
+++ src/main/java/org/asansocketserver/AsanSocketServerApplication.java
@@ -13,5 +13,6 @@ public class AsanSocketServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AsanSocketServerApplication.class, args);
     }
-    
+
+
 }
